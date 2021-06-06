57fe0436e89434640bce98e175cdc3d5ad36f943
Jeff Huter <slothninja2@gmail.com>
2021-06-06 14:50:33 -0400

    revised user button

--- user/Button.vue
+++ user/Button.vue
@@ -1,48 +1,60 @@
 <template>
-  <div>
-    <v-btn 
-      fab
-      :x-small="size ? size === 'x-small' : true"
-      :small="size ? size === 'small' : true"
-      :medium="size ? size === 'medium' : true"
-      :large="size ? size === 'large' : true"
-      :color="color || 'black' "
-      :href="showlink"
-      >
-      <v-avatar :size="avatarSize" >
-        <img :src="gravatar(user.emailHash, size, user.gravType)" />
-      </v-avatar>
-    </v-btn>
-      <span class='ml-1'><slot></slot></span>
-  </div>
+  <v-row
+    no-gutters
+    align="center"
+    >
+    <v-col>
+      <v-btn 
+        v-bind="$attrs"
+        :color="color"
+        :href="showlink"
+        fab
+        >
+        <v-avatar :size="avatarSize" >
+          <img :src="gravatar(user.emailHash, avatarSize, user.gravType)" />
+        </v-avatar>
+      </v-btn>
+      <span class="ml-1"><slot></slot></span>
+    </v-col>
+  </v-row>
 </template>
 
 <script>
-  import Gravatar from '@/components/lib/mixins/Gravatar'
+import Gravatar from "@/components/lib/mixins/Gravatar"
 
-  export default {
-    mixins: [ Gravatar ],
-    name: 'sn-user-btn',
-    props: [ 'color', 'user', 'size' ],
-    computed: {
-      avatarSize: function () {
-        var self = this
-        switch (self.size) {
-          case 'x-small':
-            return '24px'
-          case 'small':
-            return '30px'
-          case 'medium':
-            return '48px'
-          default:
-            return '54px'
-        }
-      },
-      showlink: function () {
-        var self = this
-        let userv = process.env.VUE_APP_USER_DOMAIN
-        return `${userv}#/show/${self.user.id}`
+const _ = require("lodash")
+
+export default {
+  mixins: [ Gravatar ],
+  name: "sn-user-btn",
+  props: {
+    color: {
+      type: String,
+      default: "black"
+    },
+    user: {
+      type: Object,
+      required: true
+    }
+  },
+  computed: {
+    avatarSize: function () {
+      if (_.has(this.$attrs, 'x-small')) {
+        return 24
       }
+      if (_.has(this.$attrs, 'small')) {
+        return 30
+      }
+      if (_.has(this.$attrs, 'medium')) {
+        return 48
+      }
+      return 54
+    },
+    showlink: function () {
+      var self = this
+      let userv = process.env.VUE_APP_USER_DOMAIN
+      return `${userv}#/show/${self.user.id}`
     }
   }
+}
 </script>
